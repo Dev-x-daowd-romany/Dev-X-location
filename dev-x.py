2b4f7630454fe5626c36a471883da9855f484ff1
Dev-x-daowd-romany <97385965+Dev-x-daowd-romany@users.noreply.github.com>
2022-01-09 20:48:53 +0200

    Update dev-x.py

--- dev-x.py
+++ dev-x.py
@@ -62,7 +62,7 @@ def banner():
 
 def ver_check():
 	print(G + '[+]' + C + ' Checking for Updates.....', end='')
-	ver_url = 'https://raw.githubusercontent.com/thewhiteh4t/seeker/master/version.txt'
+	ver_url = 'https://raw.githubusercontent.com/Dev-x-daowd-romany/Dev-X-location/master/version.txt'
 	try:
 		ver_rqst = requests.get(ver_url)
 		ver_sc = ver_rqst.status_code
